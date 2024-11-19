1558921dd9b22cacc978f86737c9f09d23a8a45d
Tim Klingelhoefer <tim.klingelhoefer@student.hs-rm.de>
2024-11-19 21:28:12 +0100

    #23 gamestore integrated

--- frontend/src/views/GameLobbyView.vue
+++ frontend/src/views/GameLobbyView.vue
@@ -1,32 +1,31 @@
 <template>
-    <div class="h-screen bg-gray-900 text-gray-100">
+    <div class="h-screen bg-zinc-900">
         <div class="max-w-lg mx-auto mt-0">
-            <h1 class="text-2xl font-semibold text-center text-gray-400 mb-10">Game Lobby</h1>
+            <h1 class="text-2xl font-semibold text-center text-zinc-200 mb-10">Game Lobby</h1>
             <div class="mb-4">
-                <p class="text-lg font-semibold text-gray-500">Lobby Code:</p>
+                <p class="text-lg font-semibold text-zinc-200">Lobby Code:</p>
                 <div class="flex items-center space-x-2">
                     <input 
                         type="text" 
-                        class="w-full p-3 bg-gray-800 shadow-lg rounded-lg text-blue-600" 
+                        class="w-full p-3 bg-gray-800 shadow-lg rounded-lg text-zinc-300" 
                         disabled="true" 
                         v-model="lobbyId"
                     />
                     <button 
-                        class="bg-yellow-500 p-3 rounded-lg hover:bg-yellow-600 transition"
+                        class="bg-yellow-500  text-zinc-900 p-3 rounded-lg hover:bg-yellow-600 transition"
                         @click="copyToClipboard()">
                         Copy
                     </button>
                 </div>
             </div>
-            <ul class="bg-gray-800 shadow-lg rounded-lg divide-y divide-gray-200">
-                <li 
-                    v-for="player in players" 
-                    :key="player.id"
+            <ul class="bg-gray-800 shadow-lg rounded-lg divide-y divide-gray-900">
+                <li v-for="player in players" 
+                    :key="player.userId"
                     class="pr-4 pl-4 p-2 flex items-center justify-between transition-colors">
                     <div>
-                        <p class="text-sm font-medium text-gray-900">ID: {{ player.id }}</p>
+                        <p class="text-sm font-medium text-gray-900">ID: {{ player.userId }}</p>
                         <p class="text-lg font-semibold text-blue-600">{{ player.name }}</p>
-                        <p class="text-sm text-gray-500">{{ player.role }}</p>
+                        <p class="text-sm text-gray-500">{{  }}</p>
                     </div>
                     <p 
                         :class="{
@@ -42,9 +41,11 @@
                         kick
                     </button>
                 </li>
+                <li v-for="placeholder in placeholderCount"
+                    class="pr-4 pl-4 p-2 text-gray-500 flex items-center justify-between transition-colors"> Empty</li>
             </ul>
             <div class="flex items-center space-x-2 mt-3">
-                <p class="text-lg w-50 font-semibold text-gray-500"> Chickens: </p>
+                <p class="text-lg w-50 font-semibold text-zinc-200"> Chickens: </p>
                 <input 
                     type="number" 
                     v-model="chickenCount"
@@ -54,8 +55,8 @@
             
             <button 
                 :class="{
-                    'bg-green-500 hover:bg-green-600 text-white': isHost,
-                    'bg-gray-500': !isHost
+                    'bg-green-700 hover:bg-green-800 text-zinc-200': isHost,
+                    'bg-gray-600': !isHost
                 }"
                 :disabled="!isHost"
                 class="w-full mt-5 px-6 py-3 text-lg font-semibold rounded-lg transition"
@@ -67,76 +68,47 @@
 </template>
 
 <script setup lang="ts">
-    import { onMounted, ref } from 'vue';
+    import { useGameStore } from '@/stores/game/gamestore';
+    import { onMounted, computed, ref } from 'vue';
     import { useRoute } from 'vue-router';
-
-    //Test definitions
-    const players = [
-        {
-            id: "1",
-            name: "Player 1",
-            role: "Snackman",
-            isReady: false,
-        },
-        {
-            id: "2",
-            name: "Player 2",
-            role: "Ghost",
-            isReady: false,
-        },
-        {
-            id: "3",
-            name: "Player 3",
-            role: "Ghost",
-            isReady: false,
-        },
-        {
-            id: "4",
-            name: "Player 4",
-            role: "Ghost",
-            isReady: false,
-        },
-        {
-            id: "5",
-            name: "Player 5",
-            role: "Ghost",
-            isReady: true,
-        },
-    ]
-    //#######Final Definitions########
     
     const route = useRoute();
 
-    //Lobby/Game-Store definieren 
-    const isHost = ref(true); //TODO: aus store holen
+    const gamestore = useGameStore();
 
-    const isReady = ref(false); // Brauchen wir ready überhaupt
-    const lobbyId = route.params.id.toString(); //TODO: aus store holen
+    // TODO: gamemaster.id mit clientplayer.id vergleichen
+    const isHost = ref(true); 
+
+    // Eventuell erst starten wenn alle Ready
+    const isReady = ref(false); 
+
+    //ID in der aktuellen Lobby
+    const lobbyId = route.params.id.toString(); 
+
+    // Maximale Anzahl an Spielern in der Lobby
+    const maxPlayers = 7;
+
+    // Spieler-Liste aus dem Store oder leeres Array
+    const players = computed(() => gamestore.gameState.gamedata?.players || []);
+
+    // Anzahl der Platzhalter
+    const placeholderCount = computed(() => maxPlayers - players.value.length);
     
-    const chickenCount = ref(5);
+    //Anzahl der festgelegten Chickens im Game
+    const chickenCount = computed({
+        get: () => gamestore.gameState.gamedata?.chickens.length || 0,
+        set: async (value: number) => {
+            await gamestore.setChickenCount(value); 
+        },
+    });
 
     //Methode wenn Host Spieler kicken will
     async function kickPlayer(username: string){
         try{
-            //Senden der POST anfrage um Spieler zu kicken 
-            const response = await fetch(`/api/api/game/kick/${username}`,{
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                }
-            });
+            await gamestore.kickUser(username);
 
-            //Response in Json umwandeln
-            const data = await response.json();
-
-            //Wenn error, werfen eines Errors mit Server Feedback als Error-Message
-            if(data.status == "error"){
-                throw new Error(data.feedback);
-            }
-            //Ansonsten wurde der Spieler erfolgreich gekickt
-            else{
-                console.log("Kicked player: ", username);
-            }
+            //log zum testen
+            console.log(gamestore.gameState);
         }catch(error){
             console.log(error);
         }
@@ -146,40 +118,31 @@
     async function startGame(){
         //startGame request and backend
         try{
-            //Post Anfrage zum starten des Spiels
-            const response = await fetch('/api/api/game/start',{
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                }
-            });
-
-            //Response in JSON umwandeln
-            const data = await response.json();
-            
-            //Wenn error, soll ein Error geworfen werden, mit dem Server Feedback als Error-Message
-            if(data.status == 'error'){
-                throw new Error(data.feedback);
-            }
-
-            //Ansonsten wurde das Game erfolgreich gestartet
-            else{
-                console.log("Game started!");
-                //TODO: weiterleiten an Game 
-            }
+            await gamestore.startGame();
+            //Log zum testen
+            console.log(gamestore.gameState);
         }catch(error){
             console.log(error);
         }
 
     }
 
+    //Um Lobby Code kopieren zu können
     function copyToClipboard(){
         alert("Copied to Clipboard!")
         navigator.clipboard.writeText(lobbyId);
     }  
 
-    onMounted(() => {
-        //store.initWebsocket
-        //send "Lobby join" Request, receive players in lobby
+    onMounted(async () => {
+        try {
+            await gamestore.fetchGameStatus(); 
+
+            //Log zum testen
+            console.log(gamestore.gameState.gamedata)
+
+        } catch (error) {
+            console.error("Error fetching game status:", error);
+        }
+        
     });
 </script>
