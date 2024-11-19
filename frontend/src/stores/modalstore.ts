4fa106da3326dcd1433759702c4afcf2d9a9159d
Aron-Merlin Schlegel <aronmerlin.schlegel@student.hs-rm.de>
2024-11-19 14:56:42 +0100

    Methoden openModal, closeModal und state isModalOpen in den modalstore verschoben

--- frontend/src/stores/modalstore.ts
+++ frontend/src/stores/modalstore.ts
@@ -1,1 +1,19 @@
 import { defineStore } from 'pinia';
+import { ref } from 'vue'
+
+export const useModalStore = defineStore('modal', () => {
+
+
+    const isModalOpen = ref(false);
+
+    function openModal() {
+        isModalOpen.value = true;
+    }
+
+    function closeModal() {
+        isModalOpen.value = false;
+    }
+
+    return { isModalOpen, openModal, closeModal }
+
+})
