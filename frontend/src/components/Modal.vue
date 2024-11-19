33a6481c3997a8122c5a0bee9f0c77d4a95540e6
Aron-Merlin Schlegel <aronmerlin.schlegel@student.hs-rm.de>
2024-11-19 14:42:09 +0100

    Grundgerüst für Modal.vue erstellt und neuen Ordner für Stores angelegt

---
+++ frontend/src/components/Modal.vue
@@ -0,0 +1,28 @@
+<template>
+    <div >
+        <slot name="icon"></slot>
+        <div v-if="isModalOpen">
+            <div>
+                <slot name="content"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isModalOpen = ref(false);
+
+function openModal(){
+    isModalOpen.value = true;
+}
+
+function closeModal(){
+    isModalOpen.value = false;
+}
+
+export { openModal, closeModal}
+
+</script>
