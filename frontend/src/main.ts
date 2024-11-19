40dae664c0d38d597cecf232552d1652cf0eb104
Aron-Merlin Schlegel <aronmerlin.schlegel@student.hs-rm.de>
2024-11-19 14:48:30 +0100

    Pinja installiert, in main.ts bereitgestellt und im modalstore.ts importiert

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,7 +4,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Pinia importieren
+import { createPinia } from 'pinia'
+
 const app = createApp(App)
+
+// Pinia benutzen
+app.use(createPinia())
 
 app.use(router)
 
