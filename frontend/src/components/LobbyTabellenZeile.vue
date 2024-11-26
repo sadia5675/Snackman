1d8829d205712d38501d80cbc4e7cbff451c521d
Tim Klingelhoefer <tim.klingelhoefer@student.hs-rm.de>
2024-11-26 23:23:10 +0100

    #25 Added LobbyTabellenZeile, shows a game object in a table row

---
+++ frontend/src/components/LobbyTabellenZeile.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import router from "@/router";
+import type { IGameDTD } from "@/stores/game/dtd/IGameDTD";
+import { useGameStore } from "@/stores/game/gamestore";
+
+const gameStore = useGameStore();
+
+async function joinLobby(gameId:string) {
+    gameStore.joinLobby(gameId);
+    router.push(`/lobby/${gameId}`);
+}
+
+defineProps<{
+    game: IGameDTD,
+}>();
+</script>
+
+<template>
+    <td class="px-6 py-3 text-gray-600">
+        <span class="font-medium px-6 py-3"></span> {{ game.id }}
+    </td>
+    <td class="px-6 py-3 text-gray-600">
+        <span class="font-medium px-6 py-3"></span> {{ game.gamemaster?.name || 'Unknown' }}
+    </td>
+    <td class="px-6 py-3">
+        <button
+            @click="joinLobby(game.id)"
+            class="rounded-lg bg-gray-300 hover:bg-gray-400 p-3 px-6 py-3"
+        >
+        Join
+        </button>
+    </td>
+  </template>
