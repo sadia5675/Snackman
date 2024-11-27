15f972133657ebd05f49c3e635e8bbb322198bdd
Zonera Iqbal <zonera.iqbal@student.hs-rm.de>
2024-11-27 21:48:27 +0100

    MapController

---
+++ backend/src/main/java/de/hs_rm/backend/api/MapController.java
@@ -0,0 +1,11 @@
+package de.hs_rm.backend.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/maps")
+public class MapController {
+    
+}
