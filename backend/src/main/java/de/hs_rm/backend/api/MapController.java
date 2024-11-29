9b9e7dc09ae895ff3f3912c5077da663ee69a755
Zonera Iqbal <zonera.iqbal@student.hs-rm.de>
2024-11-29 22:05:33 +0100

    MapControllerClass with PostMapping and comments

--- backend/src/main/java/de/hs_rm/backend/api/MapController.java
+++ backend/src/main/java/de/hs_rm/backend/api/MapController.java
@@ -18,6 +18,47 @@ import java.util.Map;
 //so kreifen wir auf dei Daten im frontend zu
 @RequestMapping("/api/maps")
 public class MapController {
+    @PostMapping
+    public ResponseEntity<String> saveMap(@RequestBody Map<String,Object> requestMap) {
+        //TODO: process POST request
+        
+        String mapName = (String) requestMap.get("name");
+        List <String> mapData =(List<String>)requestMap.get("tiles");
+
+        //Valideirung zum Namen der Map
+        if (mapName == null || mapName.trim().isEmpty()){
+            return ResponseEntity.badRequest().body(" The name for the Map is missing.");
+        }
+        if (mapData == null|| mapData.isEmpty()){
+            return ResponseEntity.badRequest().body("Map Data is missing");
+        }
+        //Map speichern 
+        try{
+            // die Map speichert sich in dem gegebenen Ordner verzeichnis
+            File mapFile= new File("src/main/resources/maps");
+            // wenn dieser nicht existieren sollte wird dieser ertsllt
+            if(!mapFile.exists()){
+                mapFile.mkdirs();
+            }
+
+            //Dieser Part beschäftigt sich mit der txt-Datei, dabei wird eine Json-Datei vom Frontend in einer .txt umgewandetlt
+            File file= new File (mapFile, mapName + ".txt");
+            //liest die Daten aus der json datei
+            try(BufferedWriter writer = new BufferedWriter(new FileWriter(file))){
+                //geht zeilen weise durch 
+                for(String row: mapData){
+                    //im frontend --> MapCreator ist es so dass die Maps mit walls und ways gekennzeichent worden ist--> hier werden diese dann durch leerzeichen(way) und *(wall) umgewandelt
+                    String replace= row.replace("weg", " ").replace("wall", "*");
+                    writer.write(replace);
+                    writer.newLine();
+                }
+            }//Antwort an front gesendet
+            return ResponseEntity.ok(" Die Map wurde erfolgreich gespeichert.");
+        }catch (IOException e){
+            e.printStackTrace();// fehler beim Speichern der Map: + e.getMessage()
+            return ResponseEntity.status(500).body("Somethink went Wrong :( " + e.getMessage());
+        }
+    }
 
     
 }
