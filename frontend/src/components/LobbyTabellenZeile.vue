4fd336795c658c1ed9cae845266cdf1a9ad386e9
Tim Klingelhoefer <tim.klingelhoefer@student.hs-rm.de>
2024-11-29 20:07:09 +0100

    Added emit-event on join

--- frontend/src/components/LobbyTabellenZeile.vue
+++ frontend/src/components/LobbyTabellenZeile.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
 import router from "@/router";
+import { ModalType } from "@/stores/game/dtd/EModalType";
 import type { IGameDTD } from "@/stores/game/dtd/IGameDTD";
 import { useGameStore } from "@/stores/game/gamestore";
 
 const gameStore = useGameStore();
 
-async function joinLobby(gameId:string) {
-    gameStore.joinLobby(gameId);
-    router.push(`/lobby/${gameId}`);
-}
 
 defineProps<{
     game: IGameDTD,
@@ -24,7 +21,7 @@ defineProps<{
     </td>
     <td class="px-6 py-3">
         <button
-            @click="joinLobby(game.id)"
+            @click="$emit('open-modal',ModalType.JOIN_GAME, game.id)"
             class="rounded-lg bg-gray-300 hover:bg-gray-400 p-3 px-6 py-3"
         >
         Join
