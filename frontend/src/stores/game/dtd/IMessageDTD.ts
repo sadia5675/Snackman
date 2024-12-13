3843934201313695441f57cfe4bd46af5e72cc45
Tim Klingelhoefer <tim.klingelhoefer@student.hs-rm.de>
2024-12-13 19:01:03 +0100

    Reworked DTD

--- frontend/src/stores/game/dtd/IMessageDTD.ts
+++ frontend/src/stores/game/dtd/IMessageDTD.ts
@@ -1,10 +1,11 @@
+import type { IGameDTD } from "./IGameDTD";
 import type { IPlayerDTD } from "./IPlayerDTD";
 import type { IPlayerPositionDTD } from "./IPlayerPositionDTD";
 import type {IGameState} from "@/stores/game/IGameState";
 
-export interface Message {
+export interface IMessageDTD {
     type: string;
-    feedback: IPlayerPositionDTD | IPlayerDTD[] | IGameDTD | string ;
+    feedback: IPlayerPositionDTD | IPlayerDTD[] | IGameDTD | string | IPlayerPositionDTD[];
     status: 'ok' | 'error';
     time: string;
   }
