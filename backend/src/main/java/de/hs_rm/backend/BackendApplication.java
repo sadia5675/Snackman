58ddce55006a2352106885d5b9f1554d55f791e5
Daraz, Erina <erina.daraz@student.hs-rm.de>
2025-01-08 15:02:14 +0100

    base folder creation created co programming by ly

--- backend/src/main/java/de/hs_rm/backend/BackendApplication.java
+++ backend/src/main/java/de/hs_rm/backend/BackendApplication.java
@@ -3,11 +3,31 @@ package de.hs_rm.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 @SpringBootApplication(scanBasePackages = "de.hs_rm.backend")
 public class BackendApplication {
 
 	public static void main(String[] args) {
+
+		/* if base/maps not exist, create base/maps outside of java */
+		ensureDirectoriesExist("../base/maps");
+
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	private static void ensureDirectoriesExist(String path) {
+		File directory = new File(path);
+
+		if (!directory.exists()) {
+			if (directory.mkdirs()) {
+				System.out.println("Directory created successfully: " + directory.getAbsolutePath());
+			} else {
+				System.err.println("Failed to create directory: " + directory.getAbsolutePath());
+			}
+		} else {
+			System.out.println("Directory already exists: " + directory.getAbsolutePath());
+		}
 	}
 
 }
