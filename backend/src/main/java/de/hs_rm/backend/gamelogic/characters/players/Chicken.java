e548ac87d71496f0f748f88eb33b49d636766cc5
Aron-Merlin Schlegel <aronmerlin.schlegel@student.hs-rm.de>
2025-01-09 16:39:26 +0100

    script_path hinzugefügt und move Methode erstellt

--- backend/src/main/java/de/hs_rm/backend/gamelogic/characters/players/Chicken.java
+++ backend/src/main/java/de/hs_rm/backend/gamelogic/characters/players/Chicken.java
@@ -1,12 +1,24 @@
 package de.hs_rm.backend.gamelogic.characters.players;
 
+
+
+
 public class Chicken {
     int posX;
     int posY;
+    String script_path;
     
-    public Chicken(int posX, int posY) {
+    public Chicken(int posX, int posY, String script_path) {
         this.posX = posX;
         this.posY = posY;
+        this.script_path = script_path;
+    }
+    public void move(int x, int y) {
+        this.posX = x;
+        this.posY = y;
+    }
+    public String getScript_path() {
+        return script_path;
     }
     public int getPosX() {
         return posX;
@@ -20,5 +32,6 @@ public class Chicken {
     public void setPosY(int posY) {
         this.posY = posY;
     }
+
     
 }
