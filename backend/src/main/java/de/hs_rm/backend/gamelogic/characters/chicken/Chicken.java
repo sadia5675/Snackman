377e588e273bb96ca4bb3629e509ea6392a2d3dd
Huong Ly Nguyen <huongly.nguyen@student.hs-rm.de>
2025-01-12 12:27:58 +0100

    Chicken.java von Ly und Sadia zusammenführen

--- backend/src/main/java/de/hs_rm/backend/gamelogic/characters/chicken/Chicken.java
+++ backend/src/main/java/de/hs_rm/backend/gamelogic/characters/chicken/Chicken.java
@@ -1,18 +1,44 @@
 package de.hs_rm.backend.gamelogic.characters.chicken;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.python.util.PythonInterpreter;
+import org.springframework.beans.factory.annotation.Value;
+
+import de.hs_rm.backend.gamelogic.characters.players.Egg;
+import de.hs_rm.backend.gamelogic.characters.players.FoodItems;
 import de.hs_rm.backend.gamelogic.map.PlayMap;
 
 public class Chicken {
     private int posX;
     private int posY;
     private String behaviorScript;
+    private List<Egg> eggList;
+    private int currentCalorie;
+
+    @Value("${egg.caloriesbonus}")
+    private int CALORIESBONUS_EGG;
 
     public Chicken(int posX, int posY, String behaviorScript) {
         this.posX = posX;
         this.posY = posY;
         this.behaviorScript = behaviorScript;
+                
+        this.eggList = new ArrayList<>();
+        this.currentCalorie =0;
     }
 
+    public Egg eatSnack(FoodItems item, int posX, int posY){
+        this.currentCalorie += item.getNutriScore().getCalorieBonus();
+        if(this.currentCalorie>= this.CALORIESBONUS_EGG){
+            this.currentCalorie = this.currentCalorie - this.CALORIESBONUS_EGG;
+            Egg newEgg = new Egg(posX,posY);
+            this.eggList.add(newEgg);
+            return newEgg;
+        }
+            return null;
+
+    }
 
       public void executeBehavior(PlayMap playMap) {
         /*try (PythonInterpreter pyInterp = new PythonInterpreter()) {
@@ -54,5 +80,22 @@ public class Chicken {
     public void setBehaviorScript(String behaviorScript) {
         this.behaviorScript = behaviorScript;
     }
+
+    public List<Egg> getEggList() {
+        return eggList;
+    }
+
+    public void setEggList(List<Egg> eggList) {
+        this.eggList = eggList;
+    }
+
+    public int getCurrentCalorie() {
+        return currentCalorie;
+    }
+
+    public void setCurrentCalorie(int currentCalorie) {
+        this.currentCalorie = currentCalorie;
+    }
+    
     
 }
