f972e670213c9cdea6026cfb444fe0f3d65bf63d
Huong Ly Nguyen <huongly.nguyen@student.hs-rm.de>
2025-01-14 14:26:25 +0100

    egg for Item updated

--- backend/src/main/java/de/hs_rm/backend/gamelogic/characters/players/Egg.java
+++ backend/src/main/java/de/hs_rm/backend/gamelogic/characters/players/Egg.java
@@ -2,39 +2,21 @@ package de.hs_rm.backend.gamelogic.characters.players;
 
 import org.springframework.beans.factory.annotation.Value;
 
-public class Egg {
+public class Egg extends Item{
 
     @Value("${egg.caloriesbonus}")
     private int CALORIESBONUS_EGG;
 
-    private int posX, posY;
 
     private boolean isEaten;
 
     public Egg( int x, int y){
-        this.posX=x;
-        this.posY=y;
+        super("Egg", x, y,PlayerRole.SNACKMAN);
         this.isEaten = false;
     }
 
     public int getCALORIESBONUS_EGG() {
         return CALORIESBONUS_EGG;
-    }
-
-    public int getPosX() {
-        return posX;
-    }
-
-    public void setPosX(int posX) {
-        this.posX = posX;
-    }
-
-    public int getPosY() {
-        return posY;
-    }
-
-    public void setPosY(int posY) {
-        this.posY = posY;
     }
 
     public boolean isEaten() {
@@ -43,6 +25,18 @@ public class Egg {
 
     public void setEaten(boolean isEaten) {
         this.isEaten = isEaten;
+    }
+
+    @Override
+    public void applyEffect(Player player) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'applyEffect'");
+    }
+
+    @Override
+    public char getSymbol() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSymbol'");
     }
 
     
